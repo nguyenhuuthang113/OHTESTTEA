a627bb3fbcf0c3a0375316e64e34a78f6eb4c983
Nguyễn Hữu Thắng <84012140+nguyenhuuthang113@users.noreply.github.com>
2023-08-22 02:42:59 +0700

    ok
    erors

---
+++ contact/public/js/config.js
@@ -0,0 +1,67 @@
+const __DATA__ = {
+    'config' : {
+        'title' : 'OH TEA INFO',
+        'favicon' : 'home.png',
+        'notice': '🤗Welcome To OH TEA😘'
+    },
+    'cover-image' : 'bacg.jpg',
+    'avatar-image' : 'avatar.jpg',
+    'name' : 'OH TEA', 
+    'is_tick' : true,
+    'skills' : [
+        'Developer',
+        'Designer',
+    ],
+    'bio-storys' : [
+        {
+            'image' : 'admin.png',
+            'content' : 'CONTACT 👇',
+            'author' : 'BQL OH TEA',
+        },
+    ],
+    'links' : [
+        {
+            'image' : 'facebook.jpg',
+            'title' : 'Facebook Fanpage', 
+            'show-value' : 'Contact Facebook Fanpage' ,
+            'value' : 'https://www.facebook.com/100063489161292',
+            'type' : 0,
+        },
+        // {
+        //     'image' : 'zalo.jpg',
+        //     'title' : 'Zalo Chi Nhánh Quận 3', 
+        //     'show-value' : 'Contact Me Via Zalo',
+        //     'value' : 'https://zalo.me/0784548284',
+        //     'type' : 0,
+        // },
+        // {
+        //     'image' : 'zalo.jpg',
+        //     'title' : 'Zalo Chi Nhánh Tân Bình', 
+        //     'show-value' : 'Contact Me Via Zalo',
+        //     'value' : 'https://zalo.me/0987399541',
+        //     'type' : 0,
+        // },
+        {
+            'image' : 'phone.jpg',
+            'title' : 'Hotline Chi Nhánh Quận 3', 
+            'show-value' : 'Contact Me By Phone Number',
+            'value' : 'tel:0784548284',
+            'type' : 0,
+        },
+        {
+            'image' : 'phone.jpg',
+            'title' : 'Hotline Chi Nhánh Tân Bình', 
+            'show-value' : 'Contact Me By Phone Number',
+            'value' : 'tel:0702820436',
+            'type' : 0,
+        },
+        {
+            'image' : 'gmail.jpg',
+            'title' : 'Gmail Công Việc', 
+            'show-value' : 'Contact Me Via Gmail',
+            'value' : 'mailto:oliparty2018@gmail.com',
+            'type' : 0,
+        },
+    ]
+};
+
